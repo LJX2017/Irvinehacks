3190b5d84ff7cbb92d9cc61df7c2d605aa45b650
Priyansh Shah <105874877+Priyansh4444@users.noreply.github.com>
2024-01-27 09:42:15 +0000

    GOT TEXT TO SPEECH YOOOOOOOOOOOO

--- Pronsh/NLP.py
+++ Pronsh/NLP.py
@@ -3,16 +3,18 @@ import whisper
 # pip install openai-whisper 
 # sudo apt-get install ffmpeg
 import time
+# Activation word YOOOOOOO
+def transcribe(audio_file):
+    model = whisper.load_model("medium.en")
+    result = model.transcribe(audio_file)
+    return result["text"]
 
-start_time = time.time()
-
-model = whisper.load_model("medium.en")
-result = model.transcribe("EarningsCall.wav")
-
-# Example usage
-with open("transcription.txt", "w") as f:
-    f.write(result["text"])
-end_time = time.time()
-
-execution_time = end_time - start_time
-print(f"The code executed in {execution_time} seconds")
+if __name__ == "__main__":    
+    start_time = time.time()
+    model = whisper.load_model("medium.en")
+    result = model.transcribe("EarningsCall.wav")
+    with open("transcription.txt", "w") as f:
+        f.write(result["text"])
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"The code executed in {execution_time} seconds")
