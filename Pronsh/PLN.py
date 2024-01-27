92dee5253c7ca0a8e80603dd1165dffa5d263be6
Noel2222 <145948327+Noel22222@users.noreply.github.com>
2024-01-27 10:31:39 -0800

    Modularized PLN.py
    Modularized the script into a single function, and changed some variable names

--- Pronsh/PLN.py
+++ Pronsh/PLN.py
@@ -1,28 +1,29 @@
 import requests
 
 CHUNK_SIZE = 1024
-url = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"
-# the 21m00Tcm4TlvDq8ikWAM is the person of the model
-# it has labels like american and stuff like that if we want to pick from different moods of people or different accents
-
 headers = {
-  "Accept": "audio/mpeg",
-  "Content-Type": "application/json",
-  "xi-api-key": ""
-#   this is my api key for now please dont 
-}
+    "Accept": "audio/mpeg",
+    "Content-Type": "application/json",
+    "xi-api-key": ""
+    #   this is my api key for now please dont 
+    }
 
-data = {
-  "text": "Born and raised in the charming south, I can add a touch of sweet southern hospitality to your audiobooks and podcasts",
-  "model_id": "eleven_turbo_v2",
-  "voice_settings": {
-    "stability": 0.5,
-    "similarity_boost": 0.5
-  }
-}
+def generate_audio_file(voice_id: str, prompt: str):
+    """Takes voice_id and text of what you want the voice to say, then generates an audio_output.mp3 file within the local folder"""
+    url = "https://api.elevenlabs.io/v1/text-to-speech/" + voice_id
+    # the 21m00Tcm4TlvDq8ikWAM is the person of the model (list of voice_ids can be found at https://api.elevenlabs.io/v1/voices)
+    # it has labels like american and stuff like that if we want to pick from different moods of people or different accents
 
-response = requests.post(url, json=data, headers=headers)
-with open('output.mp3', 'wb') as f:
-    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-        if chunk:
-            f.write(chunk)
+    data = {
+    "text": prompt,
+    "voice_settings": {
+        "stability": 0.5,
+        "similarity_boost": 0.5
+    }
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    with open('audio_output.mp3', 'wb') as f:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
