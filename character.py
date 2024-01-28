b27bc2fc5997ec94cfd5b2b12c0062f1cb9021b7
Jingxi Li <jingxl18@uci.edu>
2024-01-27 17:35:48 -0800

    character.py can now be used directly

--- character.py
+++ character.py
@@ -4,26 +4,59 @@ from dotenv import load_dotenv
 import llama_hub
 from llama_index import download_loader
 import json
+from llama_index import ServiceContext
+from llama_index import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
+)
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent import ReActAgent
 
+load_dotenv()
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 WikipediaReader = download_loader("WikipediaReader")
 loader = WikipediaReader()
-load_dotenv()
+
 
 class Character:
-    def __init__(self):
-        # Information about the character
+    def __init__(self, user_prompt):
+        # create character based on user prompt
         self.name = None  # Information about the character
         self.personality = None
         self.language_style = None
         self.original_character = False  # If the character is an original character
-        self.llm = Gemini(model='gemini-pro')
+        self.llm = Gemini(model='gemini-pro', safety_settings=[
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_NONE",
+            }
+            ]
+        )
         self.personal_background = None
-        # self.generate_description_from_user(user_prompt)
+        self.generate_description_from_user(user_prompt)
+        self.agent = self.create_agent(user_prompt)
+
+    def chat(self, user_prompt):
+        return self.agent.chat(user_prompt)
 
     def generate_description_from_user(self, user_prompt):
         # self.information = llama_index.get_character_info(user)
-        prompt = (
-            f"Based on the following description, {user_prompt}, does this character uniquely match a known character from games, novels, movies, or other media? Consider aspects like distinct physical features, unique abilities, specific backstory elements, and any other notable characteristics that are not commonly shared with other characters. Only answer 'yes' if you are certain the description matches a known character with these unique attributes. Otherwise, answer 'no'.")
+        prompt = f"Based on the description, ***{user_prompt}***, does this description uniquely match a known character from games, novels, movies, or other media? Answer 'yes' if you are certain the description matches a known character. Otherwise, answer 'no'."
         is_original_character = self.llm.complete(prompt).text
         print("check original character", is_original_character)
         if is_original_character == "yes":
@@ -60,11 +93,11 @@ class Character:
             self.personality = self.llm.complete(f"to the best of your knowledge describe{self.name}'s personality"
                                                  "Only provide descriptive terms."
                                                  "The character's personality is:").text
-            # background_prompt = (f"Give me a full description of {self.name}'s background, do not spare any details"
-            #                      "provide a rigorous description of at least 500 words. The character's background is:")
+            # background_prompt = (f"Give me a full description of {self.name}'s background, do not spare any
+            # details" "provide a rigorous description of at least 500 words. The character's background is:")
             # self.personal_background = self.llm.complete(background_prompt).text
 
-            documents = loader.load_data(pages=['star lord'])
+            documents = loader.load_data(pages=[self.name])
             self.personal_background = documents[0].text
             self.language_style = self.llm.complete(f"describe {self.name}'s language style in concise terms."
                                                     "The character's style is:").text
@@ -77,7 +110,37 @@ class Character:
                        "personal_background": self.personal_background,
                        "language_style": self.language_style}, f)
 
+    def create_agent(self, user_prompt):
+        # character = character.Character()
+        # self.generate_description_from_user(input("Enter a prompt for the character: "))
+        documents = SimpleDirectoryReader(
+            input_files=[f"Characters/{self.name}.txt"]
+        ).load_data()
+        embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=self.llm)
+        index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+        query_engine = index.as_query_engine()
+        # response = query_engine.query("Who is Paul Graham.")
+        query_engine_tools = [
+            QueryEngineTool(
+                query_engine=query_engine,
+                metadata=ToolMetadata(
+                    name="character_info",
+                    description=(
+                        "Get information about a character"
+                    )
+                )
+            )
+        ]
+        context = (f"You are role-playing as {self.name}."
+                   f"Your personality is {self.personality}."
+                   f"You should speak in the style of {self.language_style}.")
+        return ReActAgent.from_tools(query_engine_tools, llm=self.llm, verbose=True, context=context)
+
 
 if __name__ == "__main__":
-    character = Character()
-    character.generate_description_from_user(input("Enter a prompt: "))
+    character = Character(input("Enter a prompt: "))
+    q = input("Enter your words: ")
+    while q != "quit":
+        print(character.chat(q))
+        q = input("Enter your words: ")
